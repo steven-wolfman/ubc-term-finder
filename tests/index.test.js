89fd75ca646def0c021b0148f6dd4dc413940252
Steve Wolfman <steven.wolfman@gmail.com>
2021-03-23 06:11:02 -0700

    trying to import index to test on it!

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,11 @@
+import * as UbcTerm from "index";
+
 describe("this bogus testing unit", () => {
   test("to pass automatically", () => {
     expect(1).toBe(1);
   });
+  test("to access stuff from UbcTerm"),
+    () => {
+      expect(UbcTerm.getUbcTerm).toBeTruthy();
+    };
 });
